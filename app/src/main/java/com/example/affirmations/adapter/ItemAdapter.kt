f49c5d8ca1989f8dae9f23035064a4122bcd25ab
Sachin Koparde <contactsachinkoparde@gmail.com>
2021-01-09 16:35:31 +0530

    Updated ItemAdapter.kt

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -12,6 +13,7 @@ import com.example.affirmations.model.Affirmation
 class ItemAdapter(private val context: Context, val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
         class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
             val textView: TextView = view.findViewById(R.id.item_title)
+            val imageView: ImageView = view.findViewById(R.id.item_image)
 
         }
 
@@ -24,6 +26,7 @@ class ItemAdapter(private val context: Context, val dataset: List<Affirmation>):
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int = dataset.size
